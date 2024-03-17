c63a32bfeaf227c6549eb19ada47288bffe9a63f
Sangsriti <sangsriti14.slg@gmail.com>
2024-03-18 02:53:06 +0530

    created write component

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,11 +3,27 @@ import "./write.css"
 export default function Write() {
   return (
     <div className="write">
+        <img 
+            className="writeImg"
+            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+            alt="" 
+        />
         <form className="writeForm">
             <div className="writeFormGroup">
-                <input type="file" id="fileInput" />
-                <input type="text" placeholder="Title" />
+                <label htmlFor="fileInput">
+                   <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" style={{display:"none"}}/>
+                <input type="text" placeholder="Title" className="writeInput" and autoFocus={true}/>
             </div>
+            <div className="writeFormGroup">
+                <textarea 
+                    placeholder="Tell your story..." 
+                    type="text" 
+                    className="writeInput writeText"
+                ></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
         </form>
     </div>
   )
