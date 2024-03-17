2e4c46121eebc58d45a03cbcd65052da33860e66
Sangsriti <sangsriti14.slg@gmail.com>
2024-03-18 05:01:59 +0530

    Updated register

--- src/pages/rgst/Rgst.jsx
+++ src/pages/rgst/Rgst.jsx
@@ -5,8 +5,10 @@ export default function Rgst() {
     <div className="rgst">
         <span className="registerTitle">Register</span>
         <form className="registerForm">
+            <label>Username</label>
+            <input type="text" className="registerInput" placeholder="Enter your username..."/>
             <label>Email</label>
-            <input type="text" className="registerInput" placeholder="Enter your email"/>
+            <input type="text" className="registerInput" placeholder="Enter your email.."/>
             <label>Password</label>
             <input type="password" className="registerInput" placeholder="Enter your password.."/>
             <button className="registerButton">Register</button>
