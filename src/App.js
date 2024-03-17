502f7b9b22c3a0d07d85a1846198d620d1188c8f
Sangsriti <sangsriti14.slg@gmail.com>
2024-03-18 04:57:06 +0530

    Updated App.jsx file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import Topbar from "./components/topbar/Topbar";
 /*import Single from "./pages/single/Single";*/
 /*import Write from "./pages/write/Write";*/
-import Settings from "./pages/settings/Settings";
+/*import Settings from "./pages/settings/Settings";*/
+/*import Login from "./pages/login/Login";*/
+import Rgst from "./pages/rgst/Rgst";
+
 function App() {
   return (
     <>
       <Topbar/>
-      <Settings />
+      <Rgst/>
     </>
     
   );
