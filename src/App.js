3f620314e1f334a1728ace7d9275e5e30a6ee051
Sangsriti <sangsriti14.slg@gmail.com>
2024-03-18 06:43:40 +0530

    Updated router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-/*import Home from "./pages/home/Home";*/
+import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
-/*import Single from "./pages/single/Single";*/
-/*import Write from "./pages/write/Write";*/
-/*import Settings from "./pages/settings/Settings";*/
-/*import Login from "./pages/login/Login";*/
+/*import Post from "./components/post/Post";*/
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 import Rgst from "./pages/rgst/Rgst";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom";
 
 function App() {
+  const user=false;
   return (
-    <>
+    <Router>
       <Topbar/>
-      <Rgst/>
-    </>
+      <Routes>
+        <Route 
+          exact path="/" 
+          element={<Home />} 
+        />
+        <Route 
+          path="/register" 
+          element={user?<Home/> : <Rgst />} 
+        />
+        <Route 
+          path="/login" 
+          element={user?<Home/> : <Login />} 
+        />
+        <Route 
+          path="/write" 
+          element={user?<Write /> : <Rgst />} 
+        />
+        <Route
+          path="/settings" 
+          element={user?<Settings /> : <Rgst />} 
+        />
+        <Route 
+          path="/post/:postId" 
+          element={<Single />} 
+        />
+      </Routes>
+    </Router>
     
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,8 @@
 import "./topbar.css"
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
-  
+  const user = false;
   return (
     <div className="top">
         <div className="topLeft">
@@ -12,19 +13,44 @@ export default function Topbar() {
         </div>
         <div className="topCenter">
             <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTACT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
+                <li className="topListItem">
+                  <Link className="link" to="/" >HOME</Link>
+                </li>
+                <li className="topListItem">
+                  <Link className="link" to="/" >ABOUT</Link>
+                </li>
+                <li className="topListItem">
+                  <Link className="link" to="/" >CONTACT</Link>
+                </li>
+                <li className="topListItem">
+                  <Link className="link" to="/write" >WRITE</Link>
+                </li>
+                <li className="topListItem">
+                  {user && "LOGOUT"}
+                </li>
             </ul>
         </div>
         <div className="topRight">
-            <img 
-            className="topImage"
-              src="https://media.licdn.com/dms/image/D4D03AQF-YD27Y16fQQ/profile-displayphoto-shrink_800_800/0/1677519055058?e=2147483647&v=beta&t=-CQgle3SWV48hMY15egfZKdtJRJEwFeeB60m5pIoH34"
-              alt=""
-            />
+            {
+              user ? (
+                <img 
+                  className="topImage"
+                  src="https://media.licdn.com/dms/image/D4D03AQF-YD27Y16fQQ/profile-displayphoto-shrink_800_800/0/1677519055058?e=2147483647&v=beta&t=-CQgle3SWV48hMY15egfZKdtJRJEwFeeB60m5pIoH34"
+                  alt=""
+                />
+              ) : (
+                <ul className="topList">
+                    <li className="topListItem">
+                        <Link className="link" to="/login">LOGIN</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/register">REGISTER</Link>
+                    </li>
+                </ul>
+                
+              )
+            }
+            
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
