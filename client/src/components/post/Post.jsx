996d113f6e94535418147a9bba466bec31d46eca
Sangsriti <sangsriti14.slg@gmail.com>
2024-04-01 18:58:22 +0530

    integrated backend

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,28 +1,28 @@
 import "./post.css"
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
+      {post.photo && (
         <img
           className="postImg"
-            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
+            src={post.photo}
             alt=""
         />
+      )}
         <div className="postIntfo">
           <div className="postCats">
             <span className="postCat">Music</span>
             <span className="postCat">Life</span>
           </div>
           <span className="postTitle">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            {post.title}
           </span>
           <hr/>
-          <span className="postDate">1 hour ago</span>
+          <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="postDesc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, excepturi eveniet voluptatem voluptas odio deserunt ex possimus vel culpa veritatis quis quae, ea libero magnam quam placeat, nobis architecto. Officiis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, excepturi eveniet voluptatem voluptas odio deserunt ex possimus vel culpa veritatis quis quae, ea libero magnam quam placeat, nobis architecto. Officiis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, excepturi eveniet voluptatem voluptas odio deserunt ex possimus vel culpa veritatis quis quae, ea libero magnam quam placeat, nobis architecto. Officiis!
+          {post.desc}
         </p>
     </div>
   )
